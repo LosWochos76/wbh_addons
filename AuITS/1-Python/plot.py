06fecb03d8fbb285e078092ae2382b146cdb4c2c
Alex <alexander.stuckenholz@gmail.com>
2023-05-25 15:09:52 +0200

    Working

---
+++ AuITS/1-Python/plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# Daten für den Plot
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Linienplot erstellen
+plt.plot(x, y)
+
+# Achsen beschriften
+plt.xlabel('X-Achse')
+plt.ylabel('Y-Achse')
+
+# Plot anzeigen
+plt.show()
