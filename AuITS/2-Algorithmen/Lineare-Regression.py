a7cf2f3622db74d74557915711a460f64af445db
Alexander Stuckenholz <stuckenholz@iMac-von-Alexander.local>
2023-06-26 15:01:17 +0200

    Working

--- AuITS/2-Algorithmen/Lineare-Regression.py
+++ AuITS/2-Algorithmen/Lineare-Regression.py
@@ -11,12 +11,11 @@ if response.status_code == 200:
     with open('hw_200.csv', 'wb') as f:
         f.write(response.content)
 
-# Load the CSV file into a DataFrame
 data = pd.read_csv('hw_200.csv')
 
 # Extract the independent variable (x) and dependent variable (y) from the DataFrame
-x = data.iloc[:, 1].values.reshape(-1, 1)  # Assuming the independent variable is in 'Column1'
-y = data.iloc[:, 2].values  # Assuming the dependent variable is in 'Column2'
+x = data.iloc[:, 1].values.reshape(-1, 1) 
+y = data.iloc[:, 2].values
 
 # Create a linear regression model
 model = LinearRegression()
@@ -33,18 +32,20 @@ predicted_y = model.predict(x)
 
 # Calculate the mean squared error
 mse = mean_squared_error(y, predicted_y)
+
+# Calculate the r2-score
 r2 = r2_score(y, predicted_y)
 
 plt.scatter(x, y, color='blue', label='Tatsächliche Daten')
-plt.plot(x, predicted_y, color='red', label='Regression')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Linear Regression')
+#plt.plot(x, predicted_y, color='red', label='Regression')
+plt.xlabel('Körpergröße (Inch)')
+plt.ylabel('Gewicht (Pounds)')
+#plt.title('Linear Regression')
 plt.legend()
 
 formula_text = f'y = {a:.2f}x + {b:.2f}'
-plt.text(x.max() * 0.95, y.min() * 1.1, formula_text, color='black', fontsize=12)
-plt.text(x.max() * 0.95, y.min() * 1.05, f'mse = {mse:.2f}', color='black', fontsize=12)
-plt.text(x.max() * 0.95, y.min() * 1.0, f'r^2 = {r2:.2f}', color='black', fontsize=12)
+#plt.text(x.max() * 0.95, y.min() * 1.1, formula_text, color='black', fontsize=12)
+#plt.text(x.max() * 0.95, y.min() * 1.05, f'mse = {mse:.2f}', color='black', fontsize=12)
+#plt.text(x.max() * 0.95, y.min() * 1.0, f'r^2 = {r2:.2f}', color='black', fontsize=12)
 
 plt.show()
